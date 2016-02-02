624affbdc3246e6635bf74faea92b5d5f75899fc
pwener <phwener@gmail.com>
2016-02-02 21:03:51 -0200

    First commit

---
+++ app/src/main/java/wener/net/fraseado/model/Phrase.java
@@ -0,0 +1,42 @@
+package wener.net.fraseado.model;
+
+import java.util.Date;
+
+public class Phrase {
+    private Long id;
+    private String content;
+    private Date initialDate;
+    private Date lastEditDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getInitialDate() {
+        return initialDate;
+    }
+
+    public void setInitialDate(Date initialDate) {
+        this.initialDate = initialDate;
+    }
+
+    public Date getLastEditDate() {
+        return lastEditDate;
+    }
+
+    public void setLastEditDate(Date lastEditDate) {
+        this.lastEditDate = lastEditDate;
+    }
+}
