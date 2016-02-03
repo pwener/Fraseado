cc6dbfb589ae17c8a88697005627fc3b8070e856
pwener <phwener@gmail.com>
2016-02-03 05:45:48 -0200

    Adding fake list

--- app/src/main/java/wener/net/fraseado/MainActivity.java
+++ app/src/main/java/wener/net/fraseado/MainActivity.java
@@ -4,13 +4,11 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -47,7 +45,14 @@ public class MainActivity extends AppCompatActivity {
         });
 
         progressSaveView = findViewById(R.id.save_progress);
+
+        String[] values = {"HELLO", "WORLD", "LOREM", "IPSUM"};
+
+        // Creating adapter
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.activity_listview, values);
+
         listPhraseView = (ListView) findViewById(R.id.listPhraseView);
+        listPhraseView.setAdapter(adapter);
     }
 
     /**
